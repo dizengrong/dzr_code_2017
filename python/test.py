4987936441af4dc214053a62c2e41c5c46df03c3
dzr <dizengrong@gmail.com>
2017-09-15 18:08:20 +0800

    code

--- python/test.py
+++ python/test.py
@@ -1,23 +1,21 @@
-# this method is used for monitoring
+def my_generator():
+    yield 1
+    yield 2
+    yield 3
 
-import subprocess
-import locale
-import codecs
+def my_other_generator():
+    yield 10
+    yield 20
+    yield 30
+    yield 40
 
-mylist = []
-ps = subprocess.Popen('netstat -a', stdin=subprocess.PIPE,
-                      stdout=subprocess.PIPE, shell=True)
-while True:
-    data = ps.stdout.readline()
-    if data == b'':
-        if ps.poll() is not None:
-            break
-    else:
-        mylist.append(data.decode(codecs.lookup(
-            locale.getpreferredencoding()).name))
-        newlist = []
-        for i in mylist:
-            if i.find('192.168') > 0:
-                newlist.append(i)
-        newlist.sort()
-        print('Sum of requests from LAN:', len(newlist))
+gens = [my_generator(), my_other_generator()]
+
+while gens:
+    for g in gens[:]:
+        try:
+            n = g.next()
+        except StopIteration:
+            gens.remove(g)
+        else:
+            print n
